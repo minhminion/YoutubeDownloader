17ba747b38843a1ed0075cac0f003fb88478e6c1
minhminion <minhminion2015@gmail.com>
2020-08-27 13:51:52 +0700

    Deployed

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // const functions = require("firebase-functions");
 const app = require("express")()
-const cors = require("cors")({ origin: true })
+const cors = require("cors")
 const { getYoutubeInfo, downloadVideo } = require("./handlers/ytdl");
+const bodyParser = require("body-parser");
 
+app.use(cors({
+  origin: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.get("/youtube/:videoId", getYoutubeInfo);
 app.get("/youtube/:videoId/download", downloadVideo);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // exports.api = functions.https.onRequest((req, res) => {
 //   cors(req, res, () => {
